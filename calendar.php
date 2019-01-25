242f83435392d27d6e58e909177768cfdf18d218
Holly Hardin <Holly@Hollys-Macintosh.local>
2019-01-25 15:07:36 -0800

    PHP file for displaying a calendar

--- calendar.php
+++ calendar.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
+<!-- Authors: Holly -->
+<!-- This PHP file displays a calendar -->
 <!-- Reference: URL: https://www.w3schools.com/html/html_tables.asp -->
-<!-- Reference: Egerton, John. URL: https://stackoverflow.com/questions/7992198/html-how-to-make-2-tables-with-different-css -->
 <html>
 <body>
 
-<!-- Style for calednar 'table' -->
+<!-- Style for calendar 'table' -->
 <style>
 table { width:75%; height:75%; border-collapse: collapse; }
 th { text-align: right; }
@@ -18,6 +19,8 @@ $user = "guest";
 $pass = "guest";
 $dbname = "calendar";
 $port = "3728";
+
+# Open database connection
 $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error connecting to MySQL server.');
 session_start();
 
@@ -29,7 +32,7 @@ $query = "SELECT * FROM calendar.month WHERE month_id = ".$pm;
 $result = mysqli_query($conn, $query) or die (mysqli_error($conn));
 $prev_month = mysqli_fetch_array($result, MYSQLI_BOTH);
 
-# Query to get next month info
+# Query to get current month info
 $cm = 1;
 $_SESSION['cm'] = $cm;
 $cm = $_SESSION['cm'];
@@ -42,71 +45,43 @@ echo "<font size ='16'><b>".$month['name']."</b> ".$month['year']."</font>";
 
 # Display the day names
 echo "<table>";
-echo "<tr>";
-echo "<th>Sun</th>";
-echo "<th>Mon</th>";
-echo "<th>Tue</th>";
-echo "<th>Wed</th>";
-echo "<th>Thu</th>";
-echo "<th>Fri</th>";
-echo "<th>Sat</th>";
-echo"</tr>";
+echo   "<tr>";
+echo     "<th>Sun</th>";
+echo     "<th>Mon</th>";
+echo     "<th>Tue</th>";
+echo     "<th>Wed</th>";
+echo     "<th>Thu</th>";
+echo     "<th>Fri</th>";
+echo     "<th>Sat</th>";
+echo  "</tr>";
 
 # Display the days in the previous month
 echo "<tr>";
 for ($i = ($prev_month['num_days'] - $month['days_before']) + 1; $i <= $prev_month['num_days']; $i++)
 {
-	echo "<td><font color='grey'>".$i."</font></td>";
+    echo "<td><font color='grey'>".$i."</font></td>";
 }
+
 # Display the days in the current month
 for ($i = 1; $i <= $month['num_days']; $i++)
 {
-	if (($month['days_before'] + $i - 1) % 7 == 0)
-	{
-		echo "</tr>";
-		echo "<tr>";
-	}
-	echo "<td>".$i."</td>";
+    if (($month['days_before'] + $i - 1) % 7 == 0)
+    {
+        echo "</tr>";
+        echo "<tr>";
+    }
+    echo "<td>".$i."</td>";
 }
+
 # Display the days in the next month
 for ($i = 1; $i <= $month['days_after']; $i++)
 {
-	echo "<td><font color='grey'>".$i."</font></td>";
+    echo "<td><font color='grey'>".$i."</font></td>";
 }
 echo "</tr>";
 echo "</table>";
 
-$query = "SELECT * FROM calendar.event";
-$result = mysqli_query($conn, $query) or die (mysqli_error($conn));
-echo "Events:<br>";
-while ($row = mysqli_fetch_array($result, MYSQLI_BOTH))
-{
-        echo "Event name: ";
-        echo $row['name'];
-        echo "<br>Start date: ";
-        echo $row['start_date'];
-        echo "<br>";
-        echo "Start time: ";
-        echo $row['start_hour'];
-        echo ":";
-        if ($row['start_minute'] == 0)
-                echo "00";
-        else
-                echo $row['start_minute'];
-        echo "<br>";
-        echo "End date: ";
-        echo $row['end_date'];
-        echo "<br>";
-        echo "End time: ";
-        echo $row['end_hour'];
-        echo ":";
-        echo $row['end_minute'];
-        echo "<br>";
-        echo "Description: ";
-        echo $row['description'];
-}
-echo "<br>";
-
+# Close database connection
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>

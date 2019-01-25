85958b8f0c41b0b78bf0d076eec29cde348464d8
Holly Hardin <Holly@Hollys-Macintosh.local>
2019-01-25 15:27:46 -0800

    PHP file for going to the next month

--- NextMonth.php
+++ NextMonth.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
+<!-- Authors: Holly -->
+<!-- This PHP file displays a calendar and implements use of buttons
+     Shows the next month -->
 <!-- Reference: URL: https://www.w3schools.com/html/html_tables.asp -->
-<!-- Reference: Egerton, John. URL: https://stackoverflow.com/questions/7992198/html-how-to-make-2-tables-with-different-css -->
-<!-- Reference: https://stackoverflow.com/questions/871858/php-pass-variable-to-next-page -->
 <html>
 <body>
 
-<!-- Style for calednar 'table' -->
+<!-- Style for calendar -->
 <style>
 table { width:75%; height:75%; border-collapse: collapse; }
 th { text-align: right; }
@@ -19,17 +20,21 @@ $user = "guest";
 $pass = "guest";
 $dbname = "calendar";
 $port = "3728";
+
+# Open database connection
 $conn = mysqli_connect($server, $user, $pass, $dbname, $port) or die('Error connecting to MySQL server.');
+
+# Start new session
 session_start();
 
 # Query to get previous month info
-$pm = $_SESSION['pm'] + 1;
+$pm = $SESSION['pm'] + 1;
 $_SESSION['pm'] = $pm;
 $query = "SELECT * FROM calendar.month WHERE month_id = ".$pm;
 $result = mysqli_query($conn, $query) or die (mysqli_error($conn));
 $prev_month = mysqli_fetch_array($result, MYSQLI_BOTH);
 
-# Query to get next month info
+# Query to get current month info
 $cm = $_SESSION['cm'] + 1;
 $_SESSION['cm'] = $cm;
 $query = "SELECT * FROM calendar.month WHERE month_id = ".$cm;
@@ -49,33 +54,35 @@ echo "<th>Wed</th>";
 echo "<th>Thu</th>";
 echo "<th>Fri</th>";
 echo "<th>Sat</th>";
-echo"</tr>";
+echo  "</tr>";
 
 # Display the days in the previous month
 echo "<tr>";
-echo "<tr>";
 for ($i = ($prev_month['num_days'] - $month['days_before']) + 1; $i <= $prev_month['num_days']; $i++)
 {
-	echo "<td><font color='grey'>".$i."</font></td>";
+    echo "<td><font color='grey'>".$i."</font></td>";
 }
+
 # Display the days in the current month
 for ($i = 1; $i <= $month['num_days']; $i++)
 {
-	if (($month['days_before'] + $i - 1) % 7 == 0)
-	{
-		echo "</tr>";
-		echo "<tr>";
-	}
-	echo "<td>".$i."</td>";
+    if (($month['days_before'] + $i - 1) % 7 == 0)
+    {
+        echo "</tr>";
+        echo "<tr>";
+    }
+    echo "<td>".$i."</td>";
 }
+
 # Display the days in the next month
 for ($i = 1; $i <= $month['days_after']; $i++)
 {
-	echo "<td><font color='grey'>".$i."</font></td>";
+    echo "<td><font color='grey'>".$i."</font></td>";
 }
 echo "</tr>";
 echo "</table>";
 
+# Close database connection
 mysqli_free_result($result);
 mysqli_close($conn);
 ?>

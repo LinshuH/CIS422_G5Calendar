09ce4eae475095dcb752676be6471e2c54d4fd5e
Holly Hardin <hhardin@ix.cs.uoregon.edu>
2019-02-02 15:27:44 -0800

    Changed multi-day event to single day event

--- final/addquery.php
+++ final/addquery.php
@@ -3,7 +3,7 @@
 
 <?php
 # Query to insert an event into the database
-$sql = "INSERT INTO `calendar`.`event` (`event_id`, `name`, `start_date`, `start_hour`, `start_minute`, `end_date`, `end_hour`, `end_minute`, `description`, `priority`, `categories`) VALUES ('$event_id', '$name', '$start_date', '$start_hour', '$start_minute', '$end_date', '$end_hour', '$end_minute', '$description', '$priority', '$category');";
+$sql = "INSERT INTO `calendar`.`event` (`event_id`, `name`, `start_date`, `start_hour`, `start_minute`, `description`, `priority`, `categories`) VALUES ('$event_id', '$name', '$start_date', '$start_hour', '$start_minute', '$description', '$priority', '$category');";
 # Send query to database [0]
 $conn->query($sql);
 ?>

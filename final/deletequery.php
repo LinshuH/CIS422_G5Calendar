c997a3c393f23d5db73a6475962fdb8476002631
Holly Hardin <hhardin@ix.cs.uoregon.edu>
2019-02-02 17:05:56 -0800

    Updated comments

--- final/deletequery.php
+++ final/deletequery.php
@@ -1,4 +1,9 @@
+<!-- Authors: Holly -->
+<!-- [0]: Reference. URL: https://www.w3schools.com/php/php_mysql_insert.asp ->
+
 <?php
+# Query to delete an event from the database
 $sql = "DELETE FROM `calendar`.`event` WHERE (`event_id` = '$event_id');";
+# Send query to database [0]
 $conn->query($sql);
 ?>

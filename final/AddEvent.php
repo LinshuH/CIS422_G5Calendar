49931972de7ae438fa16bb2caec088c53f27fda4
Holly Hardin <hhardin@ix.cs.uoregon.edu>
2019-02-02 17:01:26 -0800

    Updated comments

--- final/AddEvent.php
+++ final/AddEvent.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <!-- Authors: Holly (HH), Daniel (DB) -->
-<!-- This PHP file gets information for adding an event -->
+<!-- This PHP file gets information for adding an event and sends it to AddEventToDatabase.php OR redirects to DisplayCalendar.php -->
 <!-- [0]: Reference. URL: https://www.w3schools.com/html/html_tables.asp -->
 <!-- [1]: Reference. URL: https://blackswan.ch/archives/811 -->
 <!-- [2]: Reference. URL: https://www.w3schools.com/php/func_mysqli_connect.asp -->
 <!-- [3]: Reference. URL: http://php.net/manual/en/reserved.variables.session.php -->
 <!-- [4]: Reference. URL: http://php.net/manual/en/mysqli.close.php -->
+<!-- [5]: Reference. URL: http://php.net/manual/en/mysqli.query.php -->
+<!-- [6]: Reference. URL: http://php.net/manual/en/mysqli-result.fetch-array.php -->
 <html>
 <body>
 
@@ -31,7 +33,7 @@ header('Cache-Control: no cache');
 session_cache_limiter('private_no_expire'); 
 session_start();
 
-# Pass along the index of the current month being displayed [3]
+# Get and pass along the index of the current month being displayed [3]
 $current_index = $_SESSION['current_index'];
 $_SESSION['current_index'] = $current_index;
 
